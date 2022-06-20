7207eb069e6a77095982dc558b6e7abb44cd8a64
Pieter De Bie <Pieter.DeBie@coronadirect.be>
2022-06-20 14:45:37 +0200

    created unittest for shiftFunction created test cases within a json file

--- Caesar Shift/CaesarShift.py
+++ Caesar Shift/CaesarShift.py
@@ -2,20 +2,22 @@ import string
 
 #string. contains a collection of string constants whith for example all ascii-lowercase characters, which is used in this script.
 #string constant is transformed in a list
-LETTERS = list(string.ascii_lowercase)
 
 def ShiftString(shift, str):
+    LETTERS = list(string.ascii_lowercase)
     shiftedString = ""
     for letter in str:
         #if the letter from the given string is an ascii letter -> perform shift operation
         if letter in list(string.ascii_letters):
-            oldLetterIndex = LETTERS.index(letter.lower()) #potential upercase letters are converted to lowercase
-            newLetterIndex = (oldLetterIndex + shift) % len(LETTERS) #to handle a loop around of the list, modulo (%) is used to get the remainder..blabla math
-            shiftedString += LETTERS[newLetterIndex] 
+            try:
+                oldLetterIndex = LETTERS.index(letter.lower()) #potential upercase letters are converted to lowercase
+                newLetterIndex = (oldLetterIndex + shift) % len(LETTERS) #to handle a loop around of the list, modulo (%) is used to get the remainder..blabla math
+                shiftedString += LETTERS[newLetterIndex]
+            except:
+                #catch non-integer shifts
+                shiftedString = " - ERROR: Crazy input - "
+                break
         else:
             shiftedString = " - ERROR: Crazy input - "
             break
     return shiftedString
-
-str = "abcdefghijklmnoMMMrstuvwxyz"
-print(ShiftString(-1,str))
---
+++ Caesar Shift/UnitTests.py
@@ -0,0 +1,35 @@
+from parameterized import parameterized
+import unittest
+import json
+import os
+from CaesarShift import ShiftString
+
+
+# --> RUN CMD: 'py UnitTests.py -v
+
+testCasesFile = "ShiftTestCases.json"
+
+def testCasesList():
+    # If the keys of all the testcases are in the same order, I would do the following to obtain the list:
+        # testCasesList = [list(testCase.values()) for testCase in json.load(open(testCasesFile))]
+    # However, to handle potential inconsistencies in the order of data (key+value) in ShiftTestCases.json, I order the keys first
+        # strangely; 'test_shiftString(self,.. ) expects the message 'msg' to be first. If the message is elsewhere and the assertEqual() is constructed correctly,
+        # the testcase will PASS but the displayed message is the first given parameter. Passing an other msg in self.assertEqual() doesn't change anything. 
+        # Therefor, 'name' was changed to 'a_name' as a key in the json file to be the first key in the sorted list.
+    testCases = json.load(open(testCasesFile))
+    sortedKeys = sorted(testCases[0].keys())
+    # nested listcomprehension to create a list of lists of the test-case data
+    testCasesList = [[testCase.get(key) for key in sortedKeys] for testCase in testCases]
+    return testCasesList
+
+class TestCases(unittest.TestCase):
+    # alternatively to subTest() of the unittest framework I use @parametirzed.
+    # this enables me to print and count all subtests separately, where subTests() only counted 1 test
+    @parameterized.expand(testCasesList)
+    def test_shiftString(self, msg, expected, input, shift ):
+        #print(msg)
+        self.assertEqual(ShiftString(shift, input), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
